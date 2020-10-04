27c2ae5aad696723bc7b7b805a282af92d0116a4
Gnimuc <qiyupei@gmail.com>
2020-10-04 12:00:45 +0800

    Set up Github Actions, tweak tests and misc. bugfixes

--- test/glfw.jl
+++ test/glfw.jl
@@ -1,34 +1,48 @@
+using GLFW
+using VulkanCore
+using VulkanCore.LibVulkan
+
+@assert GLFW.VulkanSupported()
+
+include("vkhelper.jl")
+
 const WIDTH = 800
 const HEIGHT = 600
 
-# fill application info
-sType = VK_STRUCTURE_TYPE_APPLICATION_INFO
-pApplicationName = pointer(b"Vulkan Instance")
-applicationVersion = convert_vk_back(UInt32, v"1.1")
-pEngineName = pointer(b"Test")
-engineVersion = convert_vk_back(UInt32, v"1.1")
-apiVersion = convert_vk_back(UInt32, v"1.1")
-appInfoRef = VkApplicationInfo(sType, C_NULL, pApplicationName, applicationVersion, pEngineName, engineVersion, apiVersion) |> Ref
+## init GLFW window
+GLFW.WindowHint(GLFW.CLIENT_API, GLFW.NO_API)    # not to create an OpenGL context
+GLFW.WindowHint(GLFW.RESIZABLE, 0)
+window = GLFW.CreateWindow(WIDTH, HEIGHT, "Vulkan")
 
-# fill create info
-sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
-flags = UInt32(0)
-pApplicationInfo = Base.unsafe_convert(Ptr{VkApplicationInfo}, appInfoRef)
-requiredExtensions = GLFW.GetRequiredInstanceExtensions()
-@static if Sys.isapple()
-    @assert ("VK_MVK_macos_surface" in requiredExtensions) || ("VK_EXT_metal_surface" in requiredExtensions)
+## init Vulkan
+## creating instance
+appInfoRef = VkApplicationInfo(
+    "Application Name: Create Instance",
+    v"1.0.0",
+    "No Engine Name",
+    v"1.0.0",
+    VK_API_VERSION_1_2,
+) |> Ref
+
+extensions = GLFW.GetRequiredInstanceExtensions()
+@test check_extensions(extensions)
+
+layers = get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON" ? String[] : ["VK_LAYER_KHRONOS_validation"]
+@test check_layers(layers)
+
+createInfoRef = VkInstanceCreateInfo(appInfoRef, layers, extensions) |> Ref
+
+instanceRef = Ref(VkInstance(C_NULL))
+result = GC.@preserve appInfoRef layers extensions vkCreateInstance(createInfoRef, C_NULL, instanceRef)
+
+@static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON"
+    @test_broken result == VK_SUCCESS
+else
+    @test result == VK_SUCCESS
 end
-enabledExtensionCount = Ref{Cuint}(0)
-ppEnabledExtensionNames = ccall((:glfwGetRequiredInstanceExtensions, GLFW.libglfw), Ptr{Cstring}, (Ref{Cuint},), enabledExtensionCount)
-enabledExtensionCount = enabledExtensionCount[]
-enabledLayerCount = Cuint(0)
-ppEnabledLayerNames = C_NULL
-createInfoRef = VkInstanceCreateInfo(sType, C_NULL, flags, pApplicationInfo, enabledLayerCount, ppEnabledLayerNames, enabledExtensionCount, ppEnabledExtensionNames) |> Ref
 
-# create instance
-instanceRef = Ref{VkInstance}(C_NULL)
-result = vkCreateInstance(createInfoRef, C_NULL, instanceRef)
-result != VK_SUCCESS && error("failed to create instance!")
-instance = instanceRef[]
-@show instance
-vkDestroyInstance(instance, C_NULL)
+## cleaning up
+if result == VK_SUCCESS
+    vkDestroyInstance(instanceRef[], C_NULL)
+end
+GLFW.DestroyWindow(window)
---
+++ test/old_tests.jl
@@ -0,0 +1,121 @@
+using VulkanCore
+using VulkanCore.LibVulkan
+using Test
+using GLFW
+
+const api = VulkanCore.vk
+
+convert_vk_back(::Type{UInt32}, version::VersionNumber) = (version.major << 22) + (version.minor << 12) + version.patch
+convert_vk(::Type{VersionNumber}, version::UInt32) = VersionNumber(UInt32(version) >> 22, (UInt32(version) >> 12) & 0x3ff, UInt32(version) & 0xfff)
+
+err = VkResult(0)
+count = Ref{Cuint}(0)
+# Scan layers
+err = api.vkEnumerateInstanceLayerProperties(count, C_NULL)
+@assert err == api.VK_SUCCESS
+global_layer_properties = Vector{api.VkLayerProperties}(undef, count[])
+err = api.vkEnumerateInstanceLayerProperties(count, global_layer_properties)
+@assert err == api.VK_SUCCESS
+
+function Base.show(io::IO, lp::api.VkLayerProperties)
+	println(io, "Layer Properties: ")
+	println(io, "    Layer Name: ", String(filter(x->x!=0, UInt8[lp.layerName...])))
+	println(io, "    Spec Version: ", convert_vk(VersionNumber, lp.specVersion))
+	println(io, "    Implementation Version: ", convert_vk(VersionNumber, lp.implementationVersion))
+	println(io, "    description: ", String(filter(x->x!=0, UInt8[lp.description...])))
+end
+for elem in global_layer_properties
+	println(elem)
+end
+
+appname = b"vulkaninfo"
+
+app_info = Ref(VkApplicationInfo(VK_STRUCTURE_TYPE_APPLICATION_INFO,
+								 C_NULL,
+								 pointer(appname),
+								 1,
+								 pointer(appname),
+								 1,
+								 convert_vk_back(UInt32, v"1.1")))
+
+inst_info = Ref(VkInstanceCreateInfo(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+								     C_NULL,
+									 UInt32(0),
+									 Base.unsafe_convert(Ptr{VkApplicationInfo}, app_info),
+									 0,
+									 C_NULL,
+									 0,
+									 C_NULL))
+
+instance = Ref{VkInstance}(C_NULL)
+
+err = vkCreateInstance(inst_info, C_NULL, instance)
+@test err == VK_SUCCESS
+println(instance)
+
+gpu_count = Ref{Cuint}(0)
+err = vkEnumeratePhysicalDevices(instance[], gpu_count, C_NULL)
+@test err == VK_SUCCESS
+devices = Array{VkPhysicalDevice}(undef, gpu_count[])
+
+err = vkEnumeratePhysicalDevices(instance[], gpu_count, devices)
+@test err == VK_SUCCESS
+
+deviceprops = Ref{VkPhysicalDeviceProperties}()
+vkGetPhysicalDeviceProperties(devices[], deviceprops)
+
+
+function Base.show(io::IO, pdsp::VkPhysicalDeviceSparseProperties)
+	println(io, "Physical device Sparse Properties: ")
+	for name in fieldnames(typeof(pdsp))
+		println(io, "    ", name, " ", getfield(pdsp, name) == 1)
+	end
+end
+function Base.show(io::IO, pdp::VkPhysicalDeviceProperties)
+	println(io, "Physical Device Properties: ")
+	println(io, "    API Version: ", convert_vk(VersionNumber, pdp.apiVersion))
+	println(io, "    Driver Version: ", convert_vk(VersionNumber, pdp.driverVersion))
+
+	println(io, "    Vendor ID ", pdp.vendorID)
+	println(io, "    Device ID: ", pdp.deviceID)
+	println(io, "    Device Type: ", pdp.deviceType)
+	println(io, "    Device Name: ", String(filter(x->x!=0, UInt8[pdp.deviceName...])))
+	println(io, "    Pipeline Cache UUID: ", String(collect(pdp.pipelineCacheUUID)))
+	println(io, "    Limits: ", pdp.limits)
+	println(io, "    Sparse Properties: \n    ", pdp.sparseProperties)
+end
+
+limitshow(x::Cuint) = Int(x)
+limitshow(x::NTuple) = "<"*join(map(limitshow, x), " ")*">"
+limitshow(x) = x
+
+function Base.show(io::IO, pdl::VkPhysicalDeviceLimits)
+	println(io, "Physical Device Limits: ")
+	for name in fieldnames(typeof(pdl))
+		println(io, "    ", name, " ", limitshow(getfield(pdl, name)))
+	end
+end
+
+println(deviceprops[])
+
+queue_count = Ref{Cuint}(0)
+vkGetPhysicalDeviceQueueFamilyProperties(devices[], queue_count, C_NULL)
+queueprops = Array{VkQueueFamilyProperties}(undef, queue_count[])
+println(queue_count[])
+vkGetPhysicalDeviceQueueFamilyProperties(devices[], queue_count, queueprops)
+println(queueprops)
+
+memprops = Ref{VkPhysicalDeviceMemoryProperties}()
+vkGetPhysicalDeviceMemoryProperties(devices[], memprops)
+
+println(memprops[])
+devicefeatures = Ref{VkPhysicalDeviceFeatures}()
+vkGetPhysicalDeviceFeatures(devices[], devicefeatures)
+
+function Base.show(io::IO, df::VkPhysicalDeviceFeatures)
+	println(io, "Physical device features: ")
+	for name in fieldnames(typeof(df))
+		println(io, "    ", name, ": ", getfield(df, name) != 1 ? "un" : "", "supported")
+	end
+end
+println(devicefeatures[])
