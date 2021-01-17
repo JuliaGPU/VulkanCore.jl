823afa59e71695bb52a35c8abc4dc22bcaecfbd1
serenity4 <cedric.bel@hotmail.fr>
2021-01-17 20:44:40 +0100

    Import SwiftShader first

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
-using VulkanCore
-using VulkanCore.LibVulkan
-using Test
-using GLFW
-
 @static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON"
     using SwiftShader_jll
     sep = Sys.iswindows() ? ';' : ':'
     old_vk_icd_filenames = get(ENV, "VK_ICD_FILENAMES", nothing)
     ENV["VK_ICD_FILENAMES"] = join(unique(filter(!isnothing, [old_vk_icd_filenames, joinpath(dirname(SwiftShader_jll.libvulkan), "vk_swiftshader_icd.json")])), sep)
 end
+
+using VulkanCore
+using VulkanCore.LibVulkan
+using Test
+using GLFW
 
 @testset "GLFW" begin
 	if GLFW.VulkanSupported()
