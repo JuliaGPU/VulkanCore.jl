e73da6e7a5009ab3a36a7892313d776184a50556
serenity4 <cedric.bel@hotmail.fr>
2021-01-17 20:48:39 +0100

    Add SwiftShader library dir to DL_LOAD_PATH

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 @static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON"
     using SwiftShader_jll
     sep = Sys.iswindows() ? ';' : ':'
-    old_vk_icd_filenames = get(ENV, "VK_ICD_FILENAMES", nothing)
-    ENV["VK_ICD_FILENAMES"] = join(unique(filter(!isnothing, [old_vk_icd_filenames, joinpath(dirname(SwiftShader_jll.libvulkan), "vk_swiftshader_icd.json")])), sep)
+	old_vk_icd_filenames = get(ENV, "VK_ICD_FILENAMES", nothing)
+	libdir = dirname(SwiftShader_jll.libvulkan)
+	ENV["VK_ICD_FILENAMES"] = join(unique(filter(!isnothing, [old_vk_icd_filenames, joinpath(libdir, "vk_swiftshader_icd.json")])), sep)
+	ENV["DL_LOAD_PATH"] = join([get(ENV, "LD_LIBRARY_PATH", []); libdir], ':')
 end
 
 using VulkanCore
