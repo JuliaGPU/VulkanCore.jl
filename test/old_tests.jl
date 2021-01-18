c40718c1a3dfdc620581d0ca0584774b67d453ed
serenity4 <cedric.bel@hotmail.fr>
2021-01-18 17:30:07 +0100

    Refactor tests, fix when more than one device present

--- test/old_tests.jl
+++ test/old_tests.jl
@@ -8,27 +8,23 @@ const api = VulkanCore.vk
 convert_vk_back(::Type{UInt32}, version::VersionNumber) = (version.major << 22) + (version.minor << 12) + version.patch
 convert_vk(::Type{VersionNumber}, version::UInt32) = VersionNumber(UInt32(version) >> 22, (UInt32(version) >> 12) & 0x3ff, UInt32(version) & 0xfff)
 
-err = VkResult(0)
-count = Ref{Cuint}(0)
+count = Ref{Cuint}()
 # Scan layers
-err = api.vkEnumerateInstanceLayerProperties(count, C_NULL)
-@assert err == api.VK_SUCCESS
-global_layer_properties = Vector{api.VkLayerProperties}(undef, count[])
-err = api.vkEnumerateInstanceLayerProperties(count, global_layer_properties)
-@assert err == api.VK_SUCCESS
+@test vkEnumerateInstanceLayerProperties(count, C_NULL) == VK_SUCCESS
+global_layer_properties = Vector{VkLayerProperties}(undef, count[])
+@test vkEnumerateInstanceLayerProperties(count, global_layer_properties) == VK_SUCCESS
 
-function Base.show(io::IO, lp::api.VkLayerProperties)
+function Base.show(io::IO, lp::VkLayerProperties)
 	println(io, "Layer Properties: ")
 	println(io, "    Layer Name: ", String(filter(x->x!=0, UInt8[lp.layerName...])))
 	println(io, "    Spec Version: ", convert_vk(VersionNumber, lp.specVersion))
 	println(io, "    Implementation Version: ", convert_vk(VersionNumber, lp.implementationVersion))
 	println(io, "    description: ", String(filter(x->x!=0, UInt8[lp.description...])))
 end
-for elem in global_layer_properties
-	println(elem)
-end
 
-appname = b"vulkaninfo"
+println.(global_layer_properties)
+
+appname = "vulkaninfo"
 
 app_info = Ref(VkApplicationInfo(VK_STRUCTURE_TYPE_APPLICATION_INFO,
 								 C_NULL,
@@ -36,7 +32,7 @@ app_info = Ref(VkApplicationInfo(VK_STRUCTURE_TYPE_APPLICATION_INFO,
 								 1,
 								 pointer(appname),
 								 1,
-								 convert_vk_back(UInt32, v"1.1")))
+								 convert_vk_back(UInt32, v"1.2")))
 
 inst_info = Ref(VkInstanceCreateInfo(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
 								     C_NULL,
@@ -47,22 +43,20 @@ inst_info = Ref(VkInstanceCreateInfo(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
 									 0,
 									 C_NULL))
 
-instance = Ref{VkInstance}(C_NULL)
+instance = Ref{VkInstance}()
 
-err = vkCreateInstance(inst_info, C_NULL, instance)
-@test err == VK_SUCCESS
+@test vkCreateInstance(inst_info, C_NULL, instance) == VK_SUCCESS
 println(instance)
 
-gpu_count = Ref{Cuint}(0)
-err = vkEnumeratePhysicalDevices(instance[], gpu_count, C_NULL)
-@test err == VK_SUCCESS
+gpu_count = Ref{Cuint}()
+@test vkEnumeratePhysicalDevices(instance[], gpu_count, C_NULL) == VK_SUCCESS
 devices = Array{VkPhysicalDevice}(undef, gpu_count[])
 
-err = vkEnumeratePhysicalDevices(instance[], gpu_count, devices)
-@test err == VK_SUCCESS
+@test vkEnumeratePhysicalDevices(instance[], gpu_count, devices) == VK_SUCCESS
 
 deviceprops = Ref{VkPhysicalDeviceProperties}()
-vkGetPhysicalDeviceProperties(devices[], deviceprops)
+device = first(devices)
+vkGetPhysicalDeviceProperties(device, deviceprops)
 
 
 function Base.show(io::IO, pdsp::VkPhysicalDeviceSparseProperties)
@@ -98,19 +92,19 @@ end
 
 println(deviceprops[])
 
-queue_count = Ref{Cuint}(0)
-vkGetPhysicalDeviceQueueFamilyProperties(devices[], queue_count, C_NULL)
+queue_count = Ref{Cuint}()
+vkGetPhysicalDeviceQueueFamilyProperties(device, queue_count, C_NULL)
 queueprops = Array{VkQueueFamilyProperties}(undef, queue_count[])
 println(queue_count[])
-vkGetPhysicalDeviceQueueFamilyProperties(devices[], queue_count, queueprops)
+vkGetPhysicalDeviceQueueFamilyProperties(device, queue_count, queueprops)
 println(queueprops)
 
 memprops = Ref{VkPhysicalDeviceMemoryProperties}()
-vkGetPhysicalDeviceMemoryProperties(devices[], memprops)
+vkGetPhysicalDeviceMemoryProperties(device, memprops)
 
 println(memprops[])
 devicefeatures = Ref{VkPhysicalDeviceFeatures}()
-vkGetPhysicalDeviceFeatures(devices[], devicefeatures)
+vkGetPhysicalDeviceFeatures(device, devicefeatures)
 
 function Base.show(io::IO, df::VkPhysicalDeviceFeatures)
 	println(io, "Physical device features: ")
