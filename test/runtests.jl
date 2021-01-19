fbdbddeeac008a14b9f078ada5c7628d24c5c514
Gnimuc <qiyupei@gmail.com>
2021-01-20 01:32:54 +0900

    prefix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,6 @@
 using Test
 using GLFW
 
-@static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON" && Sys.isapple()
-    libvk = joinpath(ENV["VULKAN_SDK"], "lib", "libvulkan.dylib")
-    ispath(libvk)
-    @show libvk
-    run(`install_name_tool -add_rpath $(libvk) $(GLFW.GLFW_jll.libglfw_path)`)
-    run(`otool -l $(GLFW.GLFW_jll.libglfw_path)`)
-end
-
 @testset "GLFW" begin
     @test GLFW.VulkanSupported()
 	# if GLFW.VulkanSupported()
