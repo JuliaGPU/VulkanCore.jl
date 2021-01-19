c6bafa9a827de0b5b95483ea302e7c9558034496
Gnimuc <qiyupei@gmail.com>
2021-01-20 00:37:24 +0900

    test GLFW

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
-using VulkanCore
-using VulkanCore.LibVulkan
+# using VulkanCore
+# using VulkanCore.LibVulkan
 using Test
 using GLFW
 
 @testset "GLFW" begin
-	if GLFW.VulkanSupported()
-		include("glfw.jl")
-	end
+    @test GLFW.VulkanSupported()
+	# if GLFW.VulkanSupported()
+	# 	include("glfw.jl")
+	# end
 end
 
-@static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "OFF"
-	include("old_tests.jl")
-end
+# @static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "OFF"
+# 	include("old_tests.jl")
+# end
