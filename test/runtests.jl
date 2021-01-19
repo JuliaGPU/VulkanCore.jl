ebbbe661701b91c58f1414b89934a4f2c8cb9325
Gnimuc <qiyupei@gmail.com>
2021-01-19 22:51:54 +0900

    try again

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,3 @@
-@static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON" && Sys.isapple()
-	import Libdl: DL_LOAD_PATH
-	push!(DL_LOAD_PATH, ENV["JULIA_VULKAN_SDK_SEARCH_PATH"])
-end
-
 using VulkanCore
 using VulkanCore.LibVulkan
 using Test
