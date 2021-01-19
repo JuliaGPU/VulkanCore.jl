05aa1e16df6248485fbbebcdd5ff954f52405ce3
Gnimuc <qiyupei@gmail.com>
2021-01-20 01:16:02 +0900

    fix-up

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,8 @@
 using Test
 using GLFW
 
-@static if Sys.isapple()
-    glfw = joinpath(GLFW.GLFW_jll.artifact_dir, "lib", "libglfw.dylib")
-    run(`install_name_tool -add_rpath $(joinpath(ENV["VULKAN_SDK"], "lib", "libvulkan.dylib")) $glfw`)
+@static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON" && Sys.isapple()
+    run(`install_name_tool -add_rpath $(joinpath(ENV["VULKAN_SDK"], "lib", "libvulkan.dylib")) $(GLFW.GLFW_jll.libglfw_path)`)
 end
 
 @testset "GLFW" begin
